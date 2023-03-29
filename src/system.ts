2f0df1891cb1f4868821fa395c9c92849284beb8
lynnhgwang <lynnhgwang@tencent.com>
2023-03-29 12:17:16 +0800

    init

---
+++ src/system.ts
@@ -0,0 +1,15 @@
+export const setting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  password: "",
+  systemRule: ""
+}
+
+export const message = `Powered by OpenAI Vercel
+- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
+
+export type Setting = typeof setting
+
+export const resetContinuousDialogue = false
